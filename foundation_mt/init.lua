93ac4ba3d9138663625d333e1de01b66b09ad868
Corey Powell <mistdragon100@gmail.com>
2020-11-07 19:02:08 -0500

    foundation_mt: Added recover_hp helper function

--- foundation_mt/init.lua
+++ foundation_mt/init.lua
@@ -1,4 +1,25 @@
+--
+-- Foundation MT, contains some various utilities for general minetest stuff
+--
 local mod = foundation.new_module("foundation_mt", "1.0.0")
+
+-- Helper function for recovering some amount of hp
+--
+-- It will return the amount that was actually recovered
+--
+-- A return of 0 means nothing was recovered
+--
+-- @spec recover_hp(ObjectRef, Integer, Term) :: Integer
+function foundation.com.recover_hp(entity, amount, reason)
+  local hp = entity:get_hp()
+  local hp_max = entity:get_properties().hp_max
+
+  local used_amount = math.min(hp + amount, hp_max) - hp
+  if used_amount > 0 then
+    entity:set_hp(hp + used_amount, reason)
+  end
+  return used_amount
+end
 
 -- A copy of minetest default's get_inventory_drops
 --
