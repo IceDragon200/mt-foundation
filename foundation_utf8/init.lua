fb17b32fe14bc2de203fba947b3c8ef5e8291e69
Corey Powell <mistdragon100@gmail.com>
2021-10-10 10:56:15 -0500

    foundation_utf8: Added basic utf8 module for extracting codepoints

---
+++ foundation_utf8/init.lua
@@ -0,0 +1,101 @@
+--
+-- UTF-8 Utility module
+--
+--   For dealing with utf-8 strings and converting codepoints
+--
+-- https://www.ietf.org/rfc/rfc3629.txt
+local mod = foundation.new_module("foundation_utf8", "1.0.0")
+
+-- @namespace foundation.com.utf8
+
+local utf8 = {}
+
+-- Returns the next codepoints start and tail position so the character can be
+-- extracted or just to determine if it actually exists
+-- The length of the character can be determined by subtracting the start from the tail + 1
+--
+-- @spec next_codepoint_pos(str: String, start: Integer): (Integer, Integer) | (nil, nil)
+function utf8.next_codepoint_pos(str, start)
+  start = start or 1
+  local byte = string.byte(str, start)
+  if byte then
+    local tail = start
+    if byte >= 0xF0 and byte <= 0xF7 then
+      -- 0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+      tail = start + 3
+    elseif byte >= 0xE0 and byte <= 0xEF then
+      -- 0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
+      tail = start + 2
+    elseif byte >= 0xC0 and byte <= 0xDF then
+      -- 0000 0080-0000 07FF | 110xxxxx 10xxxxxx
+      tail = start + 1
+    end
+    return start, tail
+  end
+  return nil, nil
+end
+
+-- Returns the next codepoint in the string, the start position can be
+-- specified to allow reading from an arbitrary position.
+--
+-- @spec next_codepoint(str: String, start?: Integer): (String, Integer) | (nil, Integer)
+function utf8.next_codepoint(str, start)
+  local tail
+  start, tail = utf8.next_codepoint_pos(str, start)
+
+  if start and tail then
+    return string.sub(str, start, tail), tail
+  end
+
+  return nil, 0
+end
+
+-- Returns a table with all the codepoints in the string
+--
+-- @spec codepoints(str: String, start: Integer): String[]
+function utf8.codepoints(str, start)
+  local codepoint
+  local tail
+  local result = {}
+  local i = 0
+
+  start = start or 1
+  codepoint, tail = utf8.next_codepoint(str, start)
+  while codepoint do
+    i = i + 1
+    result[i] = codepoint
+    codepoint, tail = utf8.next_codepoint(str, tail + 1)
+  end
+
+  return result
+end
+
+-- Reports the length of the string in terms of characters (not byte size)
+--
+-- @spec size(str: String): Integer
+function utf8.size(str)
+  local start
+  local tail
+  local size = 0
+  start, tail = utf8.next_codepoint_pos(str)
+  while start do
+    size = size + 1
+    start, tail = utf8.next_codepoint_pos(str, tail + 1)
+  end
+  return size
+end
+
+-- @alias len = size
+utf8.len = utf8.size
+
+-- @alias byte_size = .string.len
+utf8.byte_size = string.len
+
+-- @alias byte_len = byte_size
+utf8.byte_len = string.len
+
+foundation.com.utf8 = utf8
+
+if foundation.com.Luna then
+  mod:require("tests.lua")
+end
---
+++ foundation_utf8/tests.lua
@@ -0,0 +1,66 @@
+local mod = foundation.com.utf8
+local Luna = assert(foundation.com.Luna)
+
+local case = Luna:new("foundation.com.utf8")
+
+case:describe("next_codepoint/1", function (t2)
+  t2:test("reports the next codepoint in an empty string", function (t3)
+    t3:assert_eq(nil, mod.next_codepoint(""))
+  end)
+
+  t2:test("reports the next codepoint in an ASCII string", function (t3)
+    t3:assert_eq("H", mod.next_codepoint("Hello"))
+  end)
+
+  t2:test("reports the next codepoint in an UTF-8 string", function (t3)
+    t3:assert_eq("さ", mod.next_codepoint("さよなら"))
+  end)
+
+  t2:test("correctly reports the next codepoint position in ASCII string", function (t3)
+    local subject = "Hello"
+    local codepoint, tail = mod.next_codepoint(subject)
+    t3:assert_eq("H", codepoint)
+    codepoint, tail = mod.next_codepoint(subject, tail + 1)
+    t3:assert_eq("e", codepoint)
+    codepoint, tail = mod.next_codepoint(subject, tail + 1)
+    t3:assert_eq("l", codepoint)
+    codepoint, tail = mod.next_codepoint(subject, tail + 1)
+    t3:assert_eq("l", codepoint)
+    codepoint, tail = mod.next_codepoint(subject, tail + 1)
+    t3:assert_eq("o", codepoint)
+    codepoint, tail = mod.next_codepoint(subject, tail + 1)
+    t3:assert_eq(nil, codepoint)
+  end)
+end)
+
+case:describe("codepoints/1", function (t2)
+  t2:test("correctly returns all codepoints in an empty string", function (t3)
+    t3:assert_table_eq({}, mod.codepoints(""))
+  end)
+
+  t2:test("correctly returns all codepoints in an ASCII string", function (t3)
+    t3:assert_table_eq({"H", "e", "l", "l", "o"}, mod.codepoints("Hello"))
+  end)
+
+  t2:test("correctly returns all codepoints in a UTF-8 (Hiragana) string", function (t3)
+    t3:assert_table_eq({"さ", "よ", "な", "ら"}, mod.codepoints("さよなら"))
+  end)
+end)
+
+case:describe("size/1", function (t2)
+  t2:test("correctly reports size of empty string", function (t3)
+    t3:assert_eq(0, mod.size(""))
+  end)
+
+  t2:test("correctly reports size of ASCII string", function (t3)
+    t3:assert_eq(5, mod.size("Hello"))
+  end)
+
+  t2:test("correctly reports size of UTF-8 (Hiragana) string", function (t3)
+    t3:assert_eq(4, mod.size("さよなら"))
+  end)
+end)
+
+case:execute()
+case:display_stats()
+case:maybe_error()
