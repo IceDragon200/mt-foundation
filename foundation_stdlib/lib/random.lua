b9b11c729f24849987634fac5257fce26615e39f
Corey Powell <mistdragon100@gmail.com>
2021-10-19 22:58:50 -0500

    foundation_stdlib: Function specs on random_string*

--- foundation_stdlib/lib/random.lua
+++ foundation_stdlib/lib/random.lua
@@ -5,6 +5,7 @@ local STRING_POOL32 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
 local STRING_POOL36 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 local STRING_POOL62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+-- @spec random_string_from_pool(length: Integer, pool: String): String
 function foundation.com.random_string(length, pool)
   pool = pool or STRING_POOL62
   local pool_size = #pool
@@ -16,18 +17,22 @@ function foundation.com.random_string(length, pool)
   return table.concat(result)
 end
 
+-- @spec random_string16(length: Integer): String
 function foundation.com.random_string16(length)
   return foundation.com.random_string(length, STRING_POOL16)
 end
 
+-- @spec random_string32(length: Integer): String
 function foundation.com.random_string32(length)
   return foundation.com.random_string(length, STRING_POOL32)
 end
 
+-- @spec random_string36(length: Integer): String
 function foundation.com.random_string36(length)
   return foundation.com.random_string(length, STRING_POOL36)
 end
 
+-- @spec random_string62(length: Integer): String
 function foundation.com.random_string62(length)
   return foundation.com.random_string(length, STRING_POOL62)
 end
