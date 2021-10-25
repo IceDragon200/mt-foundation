2c03cf001301449e70969912ad4422ccfcca4327
Corey Powell <mistdragon100@gmail.com>
2021-10-25 14:22:11 -0500

    foundation_stdlib: Added several new functions to Rect

--- foundation_stdlib/lib/rect.lua
+++ foundation_stdlib/lib/rect.lua
@@ -1,3 +1,5 @@
+local number_round = assert(foundation.com.number_round)
+
 -- @namespace foundation.com.Rect
 
 --
@@ -7,7 +9,7 @@ local Rect = {}
 
 -- Create a new rectangle with all 4 components specified
 --
--- @spec new(Number, Number, Number, Number): Rect
+-- @spec new(x?: Number, y?: Number, w?: Number, h?: Number): Rect
 function Rect.new(x, y, w, h)
   return {
     x = x or 0,
@@ -50,7 +52,7 @@ end
 
 -- Copy an existing table as a rect
 --
--- @spec copy(Table) :: Rect
+-- @spec copy(Table): Rect
 function Rect.copy(rect)
   return {
     x = rect.x,
@@ -60,24 +62,77 @@ function Rect.copy(rect)
   }
 end
 
+-- Returns the position of the rect as a vector2
+--
+-- @since "1.10.0"
+-- @spec position(Rect): Vector2
+function Rect.position(rect)
+  return {
+    x = rect.x,
+    y = rect.y,
+  }
+end
+
+-- Returns the size of the rect as a vector2
+--
+-- @since "1.10.0"
+-- @spec size(Rect): Vector2
+function Rect.size(rect)
+  return {
+    x = rect.w,
+    y = rect.h,
+  }
+end
+
+-- @since "1.10.0"
+-- @spec area(Rect): Integer
+function Rect.area(rect)
+  return rect.w * rect.h
+end
+
+-- @since "1.10.0"
+-- @spec perimeter(Rect): Integer
+function Rect.perimeter(rect)
+  return rect.w * 2 + rect.h * 2
+end
+
+-- Determines if the rectangle is empty.
+-- A rectangle is considered empty if either its width or height are zero.
+--
+-- @since "1.10.0"
+-- @spec is_empty(Rect): Boolean
+function Rect.is_empty(rect)
+  return rect.w == 0 or rect.h == 0
+end
+
 -- Shrink the specified rect by the given horizontal and vertical size
 -- if no vertical is given, it is assumed to be the same size as the horizontal
 --
--- @spec contract(Rect, Number, Number): Rect
-function Rect.contract(rect, horz, vert)
-  vert = vert or horz
+-- @spec contract(Rect, x: Number, y: Number): Rect
+function Rect.contract(rect, x, y)
+  y = y or x
 
-  rect.x = rect.x + horz
-  rect.y = rect.y + vert
-  rect.w = rect.w - horz * 2
-  rect.h = rect.h - vert * 2
+  rect.x = rect.x + x
+  rect.y = rect.y + y
+  rect.w = rect.w - x * 2
+  rect.h = rect.h - y * 2
 
   return rect
 end
 
+-- Expands the size of the rect from the center point, inverse of contract.
+--
+-- @since "1.10.0"
+-- @spec expand(Rect, x: Number, y: Number): Rect
+function Rect.expand(rect, x, y)
+  y = y or x
+
+  return Rect.contract(rect, -x, -y)
+end
+
 -- Changes the width and height of the specified rectangle
 --
--- @spec resize(Rect, Number, Number): Rect
+-- @spec resize(Rect, w: Number, h: Number): Rect
 function Rect.resize(rect, w, h)
   rect.w = w
   rect.h = h
@@ -85,14 +140,335 @@ function Rect.resize(rect, w, h)
   return rect
 end
 
+-- Changes the x and y coords of the specified rectangle
+--
+-- @since "1.10.0"
+-- @spec move(Rect, x: Number, y: Number): Rect
+function Rect.move(rect, x, y)
+  rect.x = x
+  rect.y = y
+
+  return rect
+end
+
 -- Moves the rectangle relative by the given coords
 --
--- @spec translate(Rect, Number, Number): Rect
+-- @spec translate(Rect, x: Number, y: Number): Rect
 function Rect.translate(rect, x, y)
   rect.x = rect.x + x
   rect.y = rect.y + y
 
   return rect
+end
+
+-- Rounds all components in the rect using floor.
+--
+-- @since "1.10.0"
+-- @spec floor(rect: Rect): Rect
+function Rect.floor(rect)
+  rect.x = math.floor(rect.x)
+  rect.y = math.floor(rect.y)
+  rect.w = math.floor(rect.w)
+  rect.h = math.floor(rect.h)
+
+  return rect
+end
+
+-- Rounds all components in the rect using ceil.
+--
+-- @since "1.10.0"
+-- @spec ceil(rect: Rect): Rect
+function Rect.ceil(rect)
+  rect.x = math.ceil(rect.x)
+  rect.y = math.ceil(rect.y)
+  rect.w = math.ceil(rect.w)
+  rect.h = math.ceil(rect.h)
+
+  return rect
+end
+
+-- Rounds all components in the rect.
+--
+-- @since "1.10.0"
+-- @spec round(rect: Rect): Rect
+function Rect.round(rect)
+  rect.x = number_round(rect.x)
+  rect.y = number_round(rect.y)
+  rect.w = number_round(rect.w)
+  rect.h = number_round(rect.h)
+
+  return rect
+end
+
+-- Aligns the given rect to the parent `container` based on the given x, and y
+-- The x and y coords are values -1 to 1, where -1 means align to the
+-- left or top and 1 means to align to the right or bottom, 0 is centered.
+--
+-- @since "1.10.0"
+-- @spec align(rect: Rect, x?: -1..1, y?: -1..1, container: Rect): Rect
+function Rect.align(rect, x, y, container)
+  if x then
+    if x < 0 then
+      rect.x = container.x
+    elseif x > 0 then
+      rect.x = container.x + container.w - rect.w
+    else
+      rect.x = container.x + (container.w - rect.w) / 2
+    end
+  end
+
+  if y then
+    if y < 0 then
+      rect.y = container.y
+    elseif y > 0 then
+      rect.y = container.y + container.h - rect.h
+    else
+      rect.y = container.y + (container.h - rect.h) / 2
+    end
+  end
+
+  return rect
+end
+
+-- Splits the rectangle into a list of evenly spaced subdivisions.
+-- Either or both the x_subdivisions and y_subdivisions can be provided to split
+-- the rect.
+--
+-- @since "1.10.0"
+-- @spec subdivide(rect: Rect, x_subdivisions?: Integer, y_subdivisions?: Integer): Rect[]
+function Rect.subdivide(rect, x_subdivisions, y_subdivisions)
+  local x
+  local y
+  local w
+  local h
+  local result = {}
+
+  if x_subdivisions and y_subdivisions then
+    --
+    w = rect.w / x_subdivisions
+    h = rect.h / y_subdivisions
+
+    for i = 1,y_subdivisions do
+      for j = 1,x_subdivisions do
+        x = (j - 1) * w
+        y = (i - 1) * h
+        result[1 + (i - 1) * x_subdivisions + j - 1] = Rect.new(rect.x + x, rect.y + y, w, h)
+      end
+    end
+  elseif x_subdivisions then
+    w = rect.w / x_subdivisions
+
+    for i = 1,x_subdivisions do
+      x = (i - 1) * w
+      result[i] = Rect.new(rect.x + x, rect.y, w, rect.h)
+    end
+  elseif y_subdivisions then
+    h = rect.h / y_subdivisions
+
+    for i = 1,y_subdivisions do
+      y = (i - 1) * h
+      result[i] = Rect.new(rect.x, rect.y + y, rect.w, h)
+    end
+  else
+    error("expected either x and or y subdivisions")
+  end
+
+  return result
+end
+
+-- Slices the Rectangle on the X-axis at specified position
+--
+-- @since "1.10.0"
+-- @spec slice_x(Rect, x: Number): Rect[]
+function Rect.slice_x(rect, x)
+  --
+  local result = {}
+
+  local left = Rect.resize(Rect.copy(rect), x, rect.h)
+  local right = Rect.resize(Rect.copy(rect), math.max(rect.w - x, 0), rect.h)
+  right.x = right.x + x
+
+  result[1] = left
+  result[2] = right
+
+  return result
+end
+
+-- Slices the Rectangle on the Y-axis at specified position
+--
+-- @since "1.10.0"
+-- @spec slice_y(Rect, y: Number): Rect[]
+function Rect.slice_y(rect, y)
+  --
+  local result = {}
+
+  local up = Rect.resize(Rect.copy(rect), rect.w, y)
+  local down = Rect.resize(Rect.copy(rect), rect.w, math.max(rect.h - y, 0))
+  down.y = down.y + y
+
+  result[1] = up
+  result[2] = down
+
+  return result
+end
+
+-- Splits a rectangle into segments.
+-- If the x coord is provided it will split the rectangle horizontally into
+-- 2 segments where the second segment starts at the x position.
+--
+-- Note that the coords are relative to the rect itself.
+--
+-- @since "1.10.0"
+-- @spec slice(rect: Rect, x?: Number, y?: Number): Rect[]
+function Rect.slice(rect, x, y)
+  if x and y then
+    --
+    local result = {}
+
+    local ul = Rect.copy(rect)
+    ul.w = x
+    ul.h = y
+
+    local ur = Rect.copy(rect)
+    ur.x = ur.x + x
+    ur.w = math.max(ur.w - x, 0)
+    ur.h = y
+
+    local dl = Rect.copy(rect)
+    dl.y = dl.y + y
+    dl.w = x
+    dl.h = math.max(dl.h - y, 0)
+
+    local dr = Rect.copy(rect)
+    dr.x = dr.x + x
+    dr.y = dr.y + y
+    dr.w = math.max(dr.w - x, 0)
+    dr.h = math.max(dr.h - y, 0)
+
+    result[1] = ul
+    result[2] = ur
+    result[3] = dl
+    result[4] = dr
+
+    return result
+
+  elseif x then
+    return Rect.slice_x(rect, x)
+
+  elseif y then
+    return Rect.slice_y(rect, y)
+
+  else
+    error("expected either x and or y coordinates")
+  end
+end
+
+-- Determines if `rect` contains another rect `other`, returns true if `other`
+-- fits completely in `rect`, returns false otherwise.
+--
+-- @since "1.10.0"
+-- @spec contains(rect: Rect, other: Rect): Boolean
+function Rect.contains(rect, other)
+  local px1 = rect.x
+  local py1 = rect.y
+  local px2 = rect.x + rect.w
+  local py2 = rect.y + rect.h
+
+  local cx1 = other.x
+  local cy1 = other.y
+  local cx2 = other.x + other.w
+  local cy2 = other.y + other.h
+
+  return cx1 >= px1 and cx1 <= px2 and
+         cx2 >= px1 and cx2 <= px2 and
+         cy1 >= py1 and cy1 <= py2 and
+         cy2 >= py1 and cy2 <= py2
+end
+
+-- Determines if `rect` and `other` intersect at any point.
+--
+-- @since "1.10.0"
+-- @spec intersects(rect: Rect, other: Rect): Boolean
+function Rect.intersects(rect, other)
+  local ax1 = rect.x
+  local ay1 = rect.y
+  local ax2 = rect.x + rect.w
+  local ay2 = rect.y + rect.h
+
+  local bx1 = other.x
+  local by1 = other.y
+  local bx2 = bx1 + other.w
+  local by2 = by1 + other.h
+
+  --     child in parent
+  return (bx1 >= ax1 and bx1 <= ax2 and by1 >= ay1 and by1 <= ay2) or
+         (bx2 >= ax1 and bx2 <= ax2 and by1 >= ay1 and by1 <= ay2) or
+         (bx1 >= ax1 and bx1 <= ax2 and by2 >= ay1 and by2 <= ay2) or
+         (bx2 >= ax1 and bx2 <= ax2 and by2 >= ay1 and by2 <= ay2) or
+         -- parent in child
+         (ax1 >= bx1 and ax1 <= bx2 and ay1 >= by1 and ay1 <= by2) or
+         (ax2 >= bx1 and ax2 <= bx2 and ay1 >= by1 and ay1 <= by2) or
+         (ax1 >= bx1 and ax1 <= bx2 and ay2 >= by1 and ay2 <= by2) or
+         (ax2 >= bx1 and ax2 <= bx2 and ay2 >= by1 and ay2 <= by2)
+end
+
+function Rect.merge(...)
+  local len = select('#', ...)
+
+  local ix2
+  local iy2
+
+  local x1
+  local x2
+  local y1
+  local y2
+  local rect
+
+  for i = 1,len do
+    rect = select(i, ...)
+
+    if x1 then
+      if rect.x < x1 then
+        x1 = rect.x
+      end
+    else
+      x1 = rect.x
+    end
+
+    if y1 then
+      if rect.y < y1 then
+        y1 = rect.y
+      end
+    else
+      y1 = rect.y
+    end
+
+    ix2 = rect.x + rect.w
+    iy2 = rect.y + rect.h
+
+    if x2 then
+      if ix2 > x2 then
+        x2 = ix2
+      end
+    else
+      x2 = ix2
+    end
+
+    if y2 then
+      if iy2 > y2 then
+        y2 = iy2
+      end
+    else
+      y2 = iy2
+    end
+  end
+
+  return Rect.new(
+    x1 or 0,
+    y1 or 0,
+    (x2 or 0) - (x1 or 0),
+    (y2 or 0) - (y1 or 0)
+  )
 end
 
 foundation.com.Rect = Rect
---
+++ foundation_stdlib/tests/rect_test.lua
@@ -0,0 +1,483 @@
+local Luna = assert(foundation.com.Luna)
+local m = foundation.com.Rect
+
+local case = Luna:new("foundation.com.Rect")
+
+case:describe("new/0", function (t2)
+  t2:test("can return a zeroed rect", function (t3)
+    local r = m.new()
+
+    t3:assert_table_eq({ x = 0, y = 0, w = 0, h = 0 }, r)
+  end)
+end)
+
+case:describe("new/4", function (t2)
+  t2:test("can create rectangle with specified coords", function (t3)
+    local r = m.new(2, 4, 22, 30)
+
+    t3:assert_table_eq({ x = 2, y = 4, w = 22, h = 30 }, r)
+  end)
+end)
+
+case:describe("new_from_vec2/2", function (t2)
+  t2:test("can create a rectangle from 2 vector2s", function (t3)
+    local r = m.new_from_vec2({ x = 2, y = 4 }, { x = 22, y = 30 })
+
+    t3:assert_table_eq({ x = 2, y = 4, w = 22, h = 30 }, r)
+  end)
+end)
+
+case:describe("new_from_extents/2", function (t2)
+  t2:test("can create a rectangle from extends", function (t3)
+    local r = m.new_from_extents({ x = 2, y = 3 }, { x = 8, y = 10 })
+
+    t3:assert_table_eq({ x = 2, y = 3, w = 6, h = 7 }, r)
+  end)
+end)
+
+case:describe("copy/1", function (t2)
+  t2:test("can copy a rect", function (t3)
+    local r = m.new(2, 4, 22, 30)
+    local r2 = m.copy(r)
+
+    r.x = 8
+
+    t3:assert_table_eq({ x = 2, y = 4, w = 22, h = 30 }, r2)
+  end)
+end)
+
+case:describe("position/1", function (t2)
+  t2:test("returns the rect position as a vector2", function (t3)
+    local r = m.new(2, 3, 7, 8)
+
+    t3:assert_table_eq({ x = 2, y = 3 }, m.position(r))
+  end)
+end)
+
+case:describe("size/1", function (t2)
+  t2:test("returns the size of the rect as a vector2", function (t3)
+    local r = m.new(2, 3, 7, 8)
+
+    t3:assert_table_eq({ x = 7, y = 8 }, m.size(r))
+  end)
+end)
+
+case:describe("is_empty/1", function (t2)
+  t2:test("correctly determines a rect is not-empty", function (t3)
+    t3:refute(m.is_empty(m.new(0, 3, 7, 8)))
+    t3:refute(m.is_empty(m.new(2, 0, 7, 8)))
+    t3:refute(m.is_empty(m.new(2, 3, 7, 8)))
+  end)
+
+  t2:test("correctly determines a rect is empty", function (t3)
+    t3:assert(m.is_empty(m.new(2, 3, 0, 8)))
+    t3:assert(m.is_empty(m.new(2, 3, 7, 0)))
+  end)
+end)
+
+case:describe("contract/2", function (t2)
+  t2:test("shifts the position and shrinks the size of the rect (given only 1 size)", function (t3)
+    local r = m.new(3, 5, 7, 9)
+
+    t3:assert_table_eq({ x = 4, y = 6, w = 5, h = 7 }, m.contract(r, 1))
+  end)
+end)
+
+case:describe("contract/3", function (t2)
+  t2:test("shifts the position and shrinks the size of the rect", function (t3)
+    local r = m.new(3, 5, 7, 9)
+
+    t3:assert_table_eq({ x = 4, y = 8, w = 5, h = 3 }, m.contract(r, 1, 3))
+  end)
+end)
+
+case:describe("expand/2", function (t2)
+  t2:test("shifts the position and increases the size of the rect (given only 1 size)", function (t3)
+    local r = m.new(3, 5, 7, 9)
+
+    t3:assert_table_eq({ x = 2, y = 4, w = 9, h = 11 }, m.expand(r, 1))
+  end)
+end)
+
+case:describe("expand/3", function (t2)
+  t2:test("shifts the position and increases the size of the rect", function (t3)
+    local r = m.new(3, 5, 7, 9)
+
+    t3:assert_table_eq({ x = 2, y = 2, w = 9, h = 15 }, m.expand(r, 1, 3))
+  end)
+end)
+
+case:describe("resize/3", function (t2)
+  t2:test("can change the size of a rect", function (t3)
+    local r = m.new(3, 5, 7, 9)
+
+    t3:assert_table_eq({ x = 3, y = 5, w = 10, h = 12 }, m.resize(r, 10, 12))
+  end)
+end)
+
+case:describe("move/3", function (t2)
+  t2:test("can change the position of a rect", function (t3)
+    local r = m.new(3, 5, 7, 9)
+
+    t3:assert_table_eq({ x = 4, y = 6, w = 7, h = 9 }, m.move(r, 4, 6))
+  end)
+end)
+
+case:describe("translate/3", function (t2)
+  t2:test("can move a rectangle relative to its position", function (t3)
+    local r = m.new(3, 5, 8, 9)
+
+    t3:assert_table_eq({ x = 4, y = 7, w = 8, h = 9 }, m.translate(r, 1, 2))
+  end)
+end)
+
+case:describe("floor/1", function (t2)
+  t2:test("rounds all components of a rect using floor method", function (t3)
+    local r = m.new(3.4, 5.7, 8.9, 9.1)
+
+    t3:assert_table_eq({ x = 3, y = 5, w = 8, h = 9 }, m.floor(r))
+  end)
+end)
+
+case:describe("ceil/1", function (t2)
+  t2:test("rounds all components of a rect using ceil method", function (t3)
+    local r = m.new(3.4, 5.7, 8.9, 9.1)
+
+    t3:assert_table_eq({ x = 4, y = 6, w = 9, h = 10 }, m.ceil(r))
+  end)
+end)
+
+case:describe("round/1", function (t2)
+  t2:test("rounds all components of a rect", function (t3)
+    local r = m.new(3.4, 5.7, 8.9, 10.1)
+
+    t3:assert_table_eq({ x = 3, y = 6, w = 9, h = 10 }, m.round(r))
+  end)
+end)
+
+case:describe("align/4", function (t2)
+  t2:test("can align given rect to a container", function (t3)
+    local container = m.new(2, 3, 20, 24)
+    local child = m.new(4, 5, 7, 6)
+
+    local x_m1 = 2
+    local x_0 = 8.5
+    local x_p1 = 15
+
+    local y_m1 = 3
+    local y_0 = 12
+    local y_p1 = 21
+
+    -- no effect
+    t3:assert_table_eq({ x = 4, y = 5, w = 7, h = 6 }, m.align(child, nil, nil, container))
+
+    -- x-coord -1
+    t3:assert_table_eq({ x = x_m1, y = 5, w = 7, h = 6 }, m.align(child, -1, nil, container))
+
+    -- x-coord 0
+    t3:assert_table_eq({ x = x_0, y = 5, w = 7, h = 6 }, m.align(child, 0, nil, container))
+
+    -- x-coord 1
+    t3:assert_table_eq({ x = x_p1, y = 5, w = 7, h = 6 }, m.align(child, 1, nil, container))
+
+    -- y-coord -1
+    t3:assert_table_eq({ x = x_p1, y = y_m1, w = 7, h = 6 }, m.align(child, nil, -1, container))
+
+    -- y-coord 0
+    t3:assert_table_eq({ x = x_p1, y = y_0, w = 7, h = 6 }, m.align(child, nil, 0, container))
+
+    -- y-coord 1
+    t3:assert_table_eq({ x = x_p1, y = y_p1, w = 7, h = 6 }, m.align(child, nil, 1, container))
+
+    for y_align, y in pairs({ [-1] = y_m1, [0] = y_0, [1] = y_p1 }) do
+      for x_align, x in pairs({ [-1] = x_m1, [0] = x_0, [1] = x_p1 }) do
+        t3:assert_table_eq({ x = x, y = y, w = 7, h = 6 }, m.align(child, x_align, y_align, container))
+      end
+    end
+  end)
+end)
+
+case:describe("subdivide/3", function (t2)
+  t2:test("can subdivide a rectangle by x subdivisions", function (t3)
+    local r = m.new(2, 4, 32, 20)
+    local result = m.subdivide(r, 4, nil)
+
+    t3:assert_deep_eq(
+      {
+        {
+          x = 2,
+          y = 4,
+          w = 8,
+          h = 20,
+        },
+        {
+          x = 10,
+          y = 4,
+          w = 8,
+          h = 20,
+        },
+        {
+          x = 18,
+          y = 4,
+          w = 8,
+          h = 20,
+        },
+        {
+          x = 26,
+          y = 4,
+          w = 8,
+          h = 20,
+        },
+      },
+      result
+    )
+  end)
+
+  t2:test("can subdivide a rectangle by y subdivisions", function (t3)
+    local r = m.new(2, 4, 32, 40)
+    local result = m.subdivide(r, nil, 4)
+
+    t3:assert_deep_eq(
+      {
+        {
+          x = 2,
+          y = 4,
+          w = 32,
+          h = 10,
+        },
+        {
+          x = 2,
+          y = 14,
+          w = 32,
+          h = 10,
+        },
+        {
+          x = 2,
+          y = 24,
+          w = 32,
+          h = 10,
+        },
+        {
+          x = 2,
+          y = 34,
+          w = 32,
+          h = 10,
+        },
+      },
+      result
+    )
+  end)
+
+  t2:test("can subdivide a rectangle by both x and y subdivisions", function (t3)
+    local r = m.new(2, 4, 32, 30)
+
+    local result = m.subdivide(r, 2, 3)
+
+    t3:assert_deep_eq(
+      {
+        {
+          x = 2,
+          y = 4,
+          w = 16,
+          h = 10,
+        },
+        {
+          x = 18,
+          y = 4,
+          w = 16,
+          h = 10,
+        },
+
+        {
+          x = 2,
+          y = 14,
+          w = 16,
+          h = 10,
+        },
+        {
+          x = 18,
+          y = 14,
+          w = 16,
+          h = 10,
+        },
+
+        {
+          x = 2,
+          y = 24,
+          w = 16,
+          h = 10,
+        },
+        {
+          x = 18,
+          y = 24,
+          w = 16,
+          h = 10,
+        },
+      },
+      result
+    )
+  end)
+end)
+
+case:describe("slice/3", function (t2)
+  t2:test("can slice a rect by a x-coord", function (t3)
+    local r = m.new(3, 4, 20, 14)
+
+    t3:assert_deep_eq(
+      {
+        {
+          x = 3,
+          y = 4,
+          w = 15,
+          h = 14,
+        },
+        {
+          x = 18,
+          y = 4,
+          w = 5,
+          h = 14,
+        },
+      },
+      m.slice(r, 15, nil)
+    )
+  end)
+
+  t2:test("can slice a rect by a y-coord", function (t3)
+    local r = m.new(3, 4, 20, 14)
+
+    t3:assert_deep_eq(
+      {
+        {
+          x = 3,
+          y = 4,
+          w = 20,
+          h = 8,
+        },
+        {
+          x = 3,
+          y = 12,
+          w = 20,
+          h = 6,
+        },
+      },
+      m.slice(r, nil, 8)
+    )
+  end)
+
+  t2:test("can slice a rect by x and y coords", function (t3)
+    local r = m.new(3, 4, 20, 14)
+
+    t3:assert_deep_eq(
+      {
+        {
+          x = 3,
+          y = 4,
+          w = 15,
+          h = 8,
+        },
+        {
+          x = 18,
+          y = 4,
+          w = 5,
+          h = 8,
+        },
+        {
+          x = 3,
+          y = 12,
+          w = 15,
+          h = 6,
+        },
+        {
+          x = 18,
+          y = 12,
+          w = 5,
+          h = 6,
+        },
+      },
+      m.slice(r, 15, 8)
+    )
+  end)
+end)
+
+case:describe("contains/2", function (t2)
+  t2:test("can determine if a rect fits in another rect", function (t3)
+    local parent = m.new(2, 3, 20, 15)
+    local child = m.new(3, 4, 5, 10)
+
+    t3:assert(m.contains(parent, child))
+  end)
+
+  t2:test("will report false if the child is completely outside the parent", function (t3)
+    local parent = m.new(2, 3, 20, 15)
+    local child1 = m.new(22, 4, 5, 10)
+    local child2 = m.new(2, 16, 5, 10)
+
+    t3:refute(m.contains(parent, child1))
+    t3:refute(m.contains(parent, child2))
+  end)
+
+  t2:test("will report false if the other rect interects but doesn't completely fit in the parent", function (t3)
+    local parent = m.new(2, 3, 20, 15)
+
+    local child0 = m.new(1, 2, 22, 17) -- expanded
+    local child1 = m.new(3, 4, 5, 15) -- h overflow
+    local child2 = m.new(3, 4, 20, 10) -- w overflow
+    local child3 = m.new(2, 2, 5, 12) -- y underflow
+    local child4 = m.new(1, 3, 5, 12) -- x underflow
+
+    t3:refute(m.contains(parent, child0))
+    t3:refute(m.contains(parent, child1))
+    t3:refute(m.contains(parent, child2))
+    t3:refute(m.contains(parent, child3))
+    t3:refute(m.contains(parent, child4))
+  end)
+end)
+
+case:describe("intersects/2", function (t2)
+  t2:test("can determine if a rect fits in another rect", function (t3)
+    local parent = m.new(2, 3, 20, 15)
+    local child = m.new(3, 4, 5, 10)
+
+    t3:assert(m.intersects(parent, child))
+  end)
+
+  t2:test("will report false if the child is completely outside the parent", function (t3)
+    local parent = m.new(2, 3, 20, 15)
+    local child1 = m.new(23, 4, 5, 10)
+    local child2 = m.new(2, 19, 5, 10)
+
+    t3:refute(m.intersects(parent, child1))
+    t3:refute(m.intersects(parent, child2))
+  end)
+
+  t2:test("will report true if the other rect overlaps but doesn't completely fit in the parent", function (t3)
+    local parent = m.new(2, 3, 20, 15)
+
+    local child0 = m.new(1, 2, 22, 17) -- expanded
+    local child1 = m.new(3, 4, 5, 15) -- h overflow
+    local child2 = m.new(3, 4, 20, 10) -- w overflow
+    local child3 = m.new(2, 2, 5, 12) -- y underflow
+    local child4 = m.new(1, 3, 5, 12) -- x underflow
+
+    t3:assert(m.intersects(parent, child0))
+    t3:assert(m.intersects(parent, child1))
+    t3:assert(m.intersects(parent, child2))
+    t3:assert(m.intersects(parent, child3))
+    t3:assert(m.intersects(parent, child4))
+  end)
+end)
+
+case:describe("merge/1+", function (t2)
+  t2:test("can merge multiple rectangles together to form one encompassing one", function (t3)
+    local r1 = m.new(2, 3, 20, 15)
+    local r2 = m.new(15, 12, 16, 8)
+
+    t3:assert_table_eq({ x = 2, y = 3, w = 29, h = 17 }, m.merge(r1, r2))
+  end)
+end)
+
+case:execute()
+case:display_stats()
+case:maybe_error()
+
+error("done")
