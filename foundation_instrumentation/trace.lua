044cef2bd0452a72dc7b1b51e4f09860b96c97e4
Corey Powell <mistdragon100@gmail.com>
2022-08-05 10:58:09 -0500

    foundation_instrumentation: Trace function specs

--- foundation_instrumentation/trace.lua
+++ foundation_instrumentation/trace.lua
@@ -1,11 +1,15 @@
+-- @namespace foundation.com
+
 --
 -- A limited implementation of OpenTrace for benchmarking code paths
 --
+-- @class Trace
 local Trace = foundation.com.Class:extends("foundation.com.Trace")
 local ic = Trace.instance_class
 
 local g_span_id = 0
 
+-- @spec #initialize(name: String): void
 function ic:initialize(name)
   g_span_id = g_span_id + 1
   self.id = g_span_id
@@ -17,11 +21,13 @@ function ic:initialize(name)
   self.d = nil
 end
 
+-- @spec #clear(): void
 function ic:clear()
   self.spans = {}
   self.span_index = 0
 end
 
+-- @spec #span_start(name: String): Trace
 function ic:span_start(name)
   local span = Trace:new(name)
   self.span_index = self.span_index + 1
@@ -29,17 +35,20 @@ function ic:span_start(name)
   return span
 end
 
+-- @spec #span_end(): void
 function ic:span_end()
   self.e = minetest.get_us_time()
   self.d = self.e - self.s
 end
 
+-- @spec #span(name: String, callback: Function/0): void
 function ic:span(name, callback)
   local span = self:span_start(name)
   callback()
   span:span_end()
 end
 
+-- @spec #format_traces(prefix: String, acc: String[]): String[]
 function ic:format_traces(prefix, acc)
   acc = acc or {}
 
@@ -60,6 +69,7 @@ function ic:format_traces(prefix, acc)
   return acc
 end
 
+-- @spec #inspect(prefix: String, acc?: String[]): void
 function ic:inspect(prefix, acc)
   acc = self:format_traces(prefix, acc or {})
   print(table.concat(acc))
