fc13e36a49d756eb758b974ff32d1662793cbc2d
Corey Powell <mistdragon100@gmail.com>
2022-08-05 10:59:20 -0500

    foundation_stdlib: assert type symbol name is a string when creating new symbols

--- foundation_stdlib/lib/symbols.lua
+++ foundation_stdlib/lib/symbols.lua
@@ -2,6 +2,8 @@
 
 --
 -- Symbols are strings mapped to an integer, for... reasons
+-- If you notice that the return type is Any for id functions, that is intended.
+-- Symbols should be treated as opaque values, their underlying type should not matter.
 --
 local Symbols = {
   m_ids = 0,
@@ -9,7 +11,9 @@ local Symbols = {
   m_symbol_to_id = {},
 }
 
+-- @spec &alloc_symbol(symbol: String): Any
 function Symbols:alloc_symbol(symbol)
+  assert(type(symbol) == "string", "expected symbol to be a string")
   if self.m_symbol_to_id[symbol] then
     error("symbol already allocated `" .. symbol .. "`")
   end
@@ -21,14 +25,17 @@ function Symbols:alloc_symbol(symbol)
   return id
 end
 
+-- @spec &maybe_id_to_symbol(id: Any): String
 function Symbols:maybe_id_to_symbol(id)
   return self.m_id_to_symbol[id]
 end
 
+-- @spec &maybe_symbol_to_id(symbol: String): Any
 function Symbols:maybe_symbol_to_id(symbol)
   return self.m_symbol_to_id[symbol]
 end
 
+-- @spec &symbol_to_id(symbol: String): Any
 function Symbols:symbol_to_id(symbol)
   return self:maybe_symbol_to_id(symbol) or
          self:alloc_symbol(symbol)
