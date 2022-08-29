03a28eb0f2c2ec4acfc50afbc18b36c8753abe5c
Corey Powell <mistdragon100@gmail.com>
2022-08-28 23:32:07 -0500

    foundation_stdlib: Added new metaref functions

--- foundation_stdlib/lib/meta_ref.lua
+++ foundation_stdlib/lib/meta_ref.lua
@@ -45,3 +45,94 @@ function foundation.com.metaref_inc_int(meta, name, amount)
   meta:set_int(name, n)
   return n
 end
+
+-- @spec metaref_int_list_to_table(
+--   MetaRef,
+--   prefix: String,
+--   max: Integer
+-- ): (count: Integer, result: Table)
+function foundation.com.metaref_int_list_to_table(meta, prefix, max)
+  local count_key = prefix .. "count"
+  local count = meta:get_int(count_key)
+
+  local result = {}
+
+  if count > 0 then
+    for i = 1,math.min(count, max) do
+      result[i] = meta:get_int(prefix .. i)
+    end
+  end
+
+  return count, result
+end
+
+-- @mutative
+-- @spec metaref_int_list_pop(MetaRef, prefix: String, max: Integer): nil | Integer
+function foundation.com.metaref_int_list_pop(meta, prefix, _max)
+  local count_key = prefix .. "count"
+  local count = meta:get_int(count_key)
+
+  if count > 0 then
+    local item = meta:get_int(prefix .. count)
+    meta:set_int(count_key, count - 1)
+    return item
+  end
+
+  return nil
+end
+
+-- @spec metaref_int_list_peek(MetaRef, prefix: String, max: Integer): nil | Integer
+function foundation.com.metaref_int_list_peek(meta, prefix, _max)
+  local count_key = prefix .. "count"
+  local count = meta:get_int(count_key)
+
+  if count > 0 then
+    local item = meta:get_int(prefix .. count)
+    return item
+  end
+
+  return nil
+end
+
+--
+-- Pushes given item unto the list, if there is no space (i.e. exceeds max) nil is returned,
+-- otherwise the index of the item added is returned instead
+--
+-- @mutative
+-- @spec metaref_int_list_push(MetaRef, prefix: String, max: Integer, item: Integer): nil | Integer
+function foundation.com.metaref_int_list_push(meta, prefix, max, item)
+  local count_key = prefix .. "count"
+  local count = meta:get_int(count_key)
+
+  if count < max then
+    count = count + 1
+    meta:set_int(prefix .. count, item)
+    meta:set_int(count_key, count)
+    return count
+  end
+
+  return nil
+end
+
+-- @mutative
+-- @spec metaref_int_list_clear(MetaRef, prefix: String, max: Integer): void
+function foundation.com.metaref_int_list_clear(meta, prefix, max)
+  local count_key = prefix .. "count"
+  local count = meta:get_int(count_key)
+
+  if count > 0 then
+    for i = 1,count do
+      meta:set_int(prefix .. count, 0)
+    end
+  end
+  meta:set_int(count_key, 0)
+end
+
+-- @mutative
+-- @spec metaref_int_list_lazy_clear(MetaRef, prefix: String, max: Integer): void
+function foundation.com.metaref_int_list_lazy_clear(meta, prefix, max)
+  local count_key = prefix .. "count"
+  local count = meta:get_int(count_key)
+
+  meta:set_int(count_key, 0)
+end
---
+++ foundation_stdlib/tests/metaref_test.lua
@@ -0,0 +1,95 @@
+local Luna = assert(foundation.com.Luna)
+local subject = foundation.com
+
+local case = Luna:new("foundation.com.metaref_*")
+
+local function make_metaref()
+  local item_stack = ItemStack()
+
+  return item_stack:get_meta()
+end
+
+local TEST_PREFIX = "test_list_"
+local MAX_SIZE = 10
+
+case:describe("metaref_int_list_to_table/3", function (t2)
+  t2:test("can return a int list (is empty)", function (t3)
+    local meta = make_metaref()
+
+    local count, list =
+      subject.metaref_int_list_to_table(meta, TEST_PREFIX, MAX_SIZE)
+
+    t3:assert_table_eq(list, {})
+    t3:assert_eq(count, 0)
+  end)
+
+  t2:test("can return an int list with items", function (t3)
+    local meta = make_metaref()
+
+    for i = 1,6 do
+      subject.metaref_int_list_push(meta, TEST_PREFIX, MAX_SIZE, i)
+    end
+
+    local count, list =
+      subject.metaref_int_list_to_table(meta, TEST_PREFIX, MAX_SIZE)
+
+    t3:assert_table_eq(list, {1, 2, 3, 4, 5, 6})
+    t3:assert_eq(count, 6)
+  end)
+end)
+
+case:describe("metaref_int_list_pop/3", function (t2)
+  t2:test("returns nil if popping a non-existent item", function (t3)
+    local meta = make_metaref()
+
+    local item = subject.metaref_int_list_pop(meta, TEST_PREFIX, MAX_SIZE)
+
+    t3:refute(item)
+  end)
+
+  t2:test("correctly pops items in list", function (t3)
+    local meta = make_metaref()
+
+    local item = math.random(0xFFFF)
+
+    t3:assert(subject.metaref_int_list_push(meta, TEST_PREFIX, MAX_SIZE, item))
+
+    local item2 = subject.metaref_int_list_pop(meta, TEST_PREFIX, MAX_SIZE)
+
+    t3:assert_eq(item, item2)
+
+    item2 = subject.metaref_int_list_pop(meta, TEST_PREFIX, MAX_SIZE)
+
+    t3:refute(item2)
+  end)
+end)
+
+case:describe("metaref_int_list_peek/3", function (t2)
+  t2:test("returns nil if peeking a non-existent item", function (t3)
+    local meta = make_metaref()
+
+    local item = subject.metaref_int_list_peek(meta, TEST_PREFIX, MAX_SIZE)
+
+    t3:refute(item)
+  end)
+
+  t2:test("correctly peeks at items in list", function (t3)
+    local meta = make_metaref()
+
+    local item = math.random(0xFFFF)
+
+    t3:assert(subject.metaref_int_list_push(meta, TEST_PREFIX, MAX_SIZE, item))
+
+    local item2 = subject.metaref_int_list_peek(meta, TEST_PREFIX, MAX_SIZE)
+
+    t3:assert_eq(item, item2)
+
+    item2 = subject.metaref_int_list_peek(meta, TEST_PREFIX, MAX_SIZE)
+
+    t3:assert_eq(item, item2)
+  end)
+end)
+
+case:execute()
+case:display_stats()
+case:maybe_error()
