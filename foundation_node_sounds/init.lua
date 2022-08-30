09373613659503c63fcb124c3b760b169628f9b3
Corey Powell <mistdragon100@gmail.com>
2022-08-30 06:11:35 -0500

    foundation_node_sounds: Lines too long

--- foundation_node_sounds/init.lua
+++ foundation_node_sounds/init.lua
@@ -2,4 +2,5 @@ local mod = foundation.new_module("foundation_node_sounds", "1.2.0")
 
 mod:require("node_sounds.lua")
 
-foundation.com.node_sounds = foundation.com.NodeSoundsRegistry:new("foundation_node_sounds:node_sounds_registry")
+foundation.com.node_sounds =
+  foundation.com.NodeSoundsRegistry:new("foundation_node_sounds:node_sounds_registry")
