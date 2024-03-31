b4e76772e361207b43b103048a39cd7d090b0f29
Corey Powell <mistdragon100@gmail.com>
2024-03-31 10:36:11 -0500

    foundation_binary: Code gardening, suffix internal to_signed and to_unsigned functions with 32 (since they operate on 32bit values)

--- foundation_binary/bit.lua
+++ foundation_binary/bit.lua
@@ -60,7 +60,7 @@ for i = 0,BITS do
   BIT_TABLE[i] = math.floor(math.pow(2, i))
 end
 
-local function to_unsigned(result)
+local function to_unsigned32(result)
   if result < 0 then
     return UINT32_MAX + result + 1
   else
@@ -70,14 +70,14 @@ end
 
 do
   local res
-  res = to_unsigned(-1)
+  res = to_unsigned32(-1)
   assert(res == 0xFFFFFFFF, "expected " .. res .. " to be equal to 0xFFFFFFFF")
 
-  res = to_unsigned(-2)
+  res = to_unsigned32(-2)
   assert(res == 0xFFFFFFFE, "expected " .. res .. " to be equal to 0xFFFFFFFE")
 end
 
-local function to_signed(value)
+local function to_signed32(value)
   if value > INT32_MAX then
     return value - UINT32_MAX - 1
   else
@@ -87,16 +87,16 @@ end
 
 do
   local res
-  res = to_signed(0xFFFFFFFF)
+  res = to_signed32(0xFFFFFFFF)
   assert(res == -1, "expected " .. res .. " to be equal to -1")
-  res = to_signed(0xFFFFFFFE)
+  res = to_signed32(0xFFFFFFFE)
   assert(res == -2, "expected " .. res .. " to be equal to -2")
 end
 
-local function to_unsigned_list(list)
+local function to_unsigned32_list(list)
   local result = {}
   for i, v in ipairs(list) do
-    result[i] = to_unsigned(v)
+    result[i] = to_unsigned32(v)
   end
   return result
 end
@@ -109,7 +109,7 @@ local function tohex(x, b)
     b = -b
     ht = UHEX_TABLE
   end
-  local y = to_unsigned(x)
+  local y = to_unsigned32(x)
   local result = {}
   for i = 1,math.floor(b/2) do
     local byte = y % 256
@@ -129,7 +129,7 @@ end
 
 local function uband(...)
   local result = 0
-  local v = to_unsigned_list({...})
+  local v = to_unsigned32_list({...})
   local j = #v
 
   for bit_index = 0,(BITS-1) do
@@ -153,7 +153,7 @@ end
 
 local function ubnot(x)
   local result = 0
-  local y = to_unsigned(x)
+  local y = to_unsigned32(x)
   for bit_index = 0,(BITS-1) do
     local base = y % 2
     y = math.floor(y / 2)
@@ -166,7 +166,7 @@ end
 
 local function ubor(...)
   local result = 0
-  local v = to_unsigned_list({...})
+  local v = to_unsigned32_list({...})
   local j = #v
 
   for bit_index = 0,(BITS-1) do
@@ -191,7 +191,7 @@ end
 
 local function ubxor(...)
   local result = 0
-  local v = to_unsigned_list({...})
+  local v = to_unsigned32_list({...})
   local j = #v
 
   for bit_index = 0,(BITS-1) do
@@ -216,7 +216,7 @@ end
 
 local function ulshift(x, n)
   assert(n >= 0)
-  local result = to_unsigned(x)
+  local result = to_unsigned32(x)
   for _ = 1,n do
     result = math.floor(result * 2)
   end
@@ -225,7 +225,7 @@ end
 
 local function urshift(x, n)
   assert(n >= 0)
-  local result = to_unsigned(x)
+  local result = to_unsigned32(x)
   for _ = 1,n do
     result = math.floor(result / 2)
   end
@@ -234,7 +234,7 @@ end
 
 local function urol(x, n)
   assert(n >= 0)
-  local y = to_unsigned(x)
+  local y = to_unsigned32(x)
   local result = 0
   for bit_index = 0,(BITS-1) do
     local b = y % 2
@@ -248,7 +248,7 @@ end
 
 local function uror(x, n)
   assert(n >= 0)
-  local y = to_unsigned(x)
+  local y = to_unsigned32(x)
   local result = 0
   for bit_index = 0,(BITS-1) do
     local b = y % 2
@@ -262,7 +262,7 @@ end
 
 local function ubswap(x)
   local a, b, c, d
-  local y = to_unsigned(x)
+  local y = to_unsigned32(x)
   a = y % 256
   y = math.floor(y / 256)
   b = y % 256
@@ -275,43 +275,43 @@ local function ubswap(x)
 end
 
 local function arshift(x, n)
-  return to_signed(uarshift(x, n))
+  return to_signed32(uarshift(x, n))
 end
 
 local function band(...)
-  return to_signed(uband(...))
+  return to_signed32(uband(...))
 end
 
 local function bnot(x)
-  return to_signed(ubnot(x))
+  return to_signed32(ubnot(x))
 end
 
 local function bor(...)
-  return to_signed(ubor(...))
+  return to_signed32(ubor(...))
 end
 
 local function bxor(...)
-  return to_signed(ubxor(...))
+  return to_signed32(ubxor(...))
 end
 
 local function lshift(x, n)
-  return to_signed(ulshift(x, n))
+  return to_signed32(ulshift(x, n))
 end
 
 local function rshift(x, n)
-  return to_signed(urshift(x, n))
+  return to_signed32(urshift(x, n))
 end
 
 local function rol(x, n)
-  return to_signed(urol(x, n))
+  return to_signed32(urol(x, n))
 end
 
 local function ror(x, n)
-  return to_signed(uror(x, n))
+  return to_signed32(uror(x, n))
 end
 
 local function bswap(x)
-  return to_signed(ubswap(x))
+  return to_signed32(ubswap(x))
 end
 
 foundation.com.local_bit = {}
